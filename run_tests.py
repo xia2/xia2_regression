757727d449a23e99f434eeb8e79c715240d1e0b4
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-11-19 17:16:28 +0000

    add basic test to make sure xia2 runs on the mad_example test dataset

---
+++ run_tests.py
@@ -0,0 +1,25 @@
+# xia2 tests - these may require the xia2_regression or dials_regression
+# repositories to be available...
+
+from __future__ import division
+from libtbx import test_utils
+import libtbx.load_env
+
+from xia2.Handlers.CommandLine import CommandLine
+from xia2.Handlers.Flags import Flags
+print Flags.get_parallel()
+Flags.set_parallel(1)
+print Flags.get_parallel()
+
+tst_list = (
+    ["$D/test/command_line/tst_xia2.py", "1"],
+)
+
+def run () :
+
+  build_dir = libtbx.env.under_build("xia2")
+  dist_dir = libtbx.env.dist_path("xia2")
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
---
+++ test/command_line/tst_xia2.py
@@ -0,0 +1,51 @@
+from __future__ import division
+
+import os
+
+import libtbx.load_env
+from libtbx import easy_run
+#from libtbx.test_utils import approx_equal
+from libtbx.test_utils import open_tmp_directory
+
+xia2_regression = libtbx.env.find_in_repositories(
+  relative_path="xia2_regression",
+  test=os.path.isdir)
+
+def exercise_1():
+  cwd = os.path.abspath(os.curdir)
+  tmp_dir = os.path.abspath(open_tmp_directory())
+  os.chdir(tmp_dir)
+
+  data_dir = os.path.join(xia2_regression, "test_data", "mad_example")
+  assert os.path.exists(data_dir)
+  options = ['-2d', 'nproc=1']
+  cmd = "xia2 %s %s" %(" ".join(options), data_dir)
+  print cmd
+  result = easy_run.fully_buffered(command=cmd).raise_if_errors()
+  #result.show_stdout()
+  assert os.path.exists(
+    os.path.join(tmp_dir, "DataFiles/AUTOMATIC_DEFAULT_scaled_WAVE1.sca"))
+  assert os.path.exists(
+    os.path.join(tmp_dir, "DataFiles/AUTOMATIC_DEFAULT_scaled_WAVE2.sca"))
+  assert os.path.exists(
+    os.path.join(tmp_dir, "DataFiles/AUTOMATIC_DEFAULT_scaled_unmerged_WAVE1.sca"))
+  assert os.path.exists(
+    os.path.join(tmp_dir, "DataFiles/AUTOMATIC_DEFAULT_scaled_unmerged_WAVE2.sca"))
+  # XXX should probably test the data quality here too somehow
+
+
+def run(args):
+  exercises = (exercise_1, )
+  if len(args):
+    args = [int(arg) for arg in args]
+    for arg in args: assert arg > 0
+    exercises = [exercises[arg-1] for arg in args]
+
+  for exercise in exercises:
+    exercise()
+
+if __name__ == '__main__':
+  import sys
+  from libtbx.utils import show_times_at_exit
+  show_times_at_exit()
+  run(sys.argv[1:])
