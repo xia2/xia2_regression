0b7fdae59f60f2e84740b82f645819328e7dfc1f
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-03-12 16:26:32 +0000

    allow some tolerance in CC half also

--- test/xia2/__init__.py
+++ test/xia2/__init__.py
@@ -36,7 +36,7 @@ def run_xia2(command_line_args, expected_summary, expected_data_files=[]):
         assert approx_equal(values_summary, values_expected, eps=5e-1), (line, expected)
       elif ('resolution limit' in line):
         assert approx_equal(values_summary, values_expected, eps=5e-2), (line, expected)
-      elif ('Rmerge' in line):
+      elif ('Rmerge' in line or 'CC half' in line):
         assert approx_equal(values_summary, values_expected, eps=2e-3), (line, expected)
       else:
         assert not show_diff(line, expected)
