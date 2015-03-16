d8d7f39587ea62a8e9db6259c0ebc2459ec5523c
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-03-16 18:08:04 +0000

    allow minor differences in multiplicities

--- test/xia2/__init__.py
+++ test/xia2/__init__.py
@@ -50,6 +50,9 @@ def run_xia2(command_line_args, expected_summary, expected_data_files=[]):
       elif ('CC half' in line):
         assert approx_equal(
           values_summary, values_expected, eps=2e-2), (line, expected)
+      elif ('Multiplicity' in line):
+        assert approx_equal(
+          values_summary, values_expected, eps=2e-1), (line, expected)
       else:
         assert not show_diff(line, expected)
 
