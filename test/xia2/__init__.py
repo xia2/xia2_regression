b8e082c372ea2478757671cf4e757c6ddbc65158
rjgildea <richard.gildea@diamond.ac.uk>
2016-04-13 12:05:59 +0100

    check for presence of xia2.html file

--- test/xia2/__init__.py
+++ test/xia2/__init__.py
@@ -19,7 +19,9 @@ def run_xia2(command_line_args, expected_summary, expected_data_files=[]):
     result.show_stdout()
     with open(error_file, 'rb') as f:
       print f.read()
-  summary_file = os.path.join(tmp_dir, 'xia2-summary.dat')
+  html_file = os.path.join(tmp_dir, 'xia2.html')
+  assert os.path.exists(html_file), "xia2.html not present after execution"
+  html_file = os.path.join(tmp_dir, 'xia2-summary.dat')
   assert os.path.exists(summary_file), "xia2-summary.dat not present after execution"
   expected_summary_lines = expected_summary.split('\n')
   summary_text = open(summary_file, 'rb').read()
