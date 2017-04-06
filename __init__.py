641e053b743470978dd9dc65ea748d8da53aa2c9
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-04-06 16:01:17 +0100

    I can't believe it's not optional

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ try:
 except Exception:
   ext = None
 else:
-  ext = boost.python.import_ext("xia2_regression_ext", optional = False)
+  ext = boost.python.import_ext("xia2_regression_ext", optional=True)
 
 if not ext is None:
   from xia2_regression_ext import *
