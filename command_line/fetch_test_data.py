4ba7cb9d21ce90757b8a63cce286a16a7291664a
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-05-10 11:01:48 +0100

    more Windows fixing

--- command_line/fetch_test_data.py
+++ command_line/fetch_test_data.py
@@ -1,10 +1,11 @@
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 
 import sys
 
-from xia2_regression.util.fetch_test_data import fetch_test_data
+if __name__ == '__main__':
+  from xia2_regression.util.fetch_test_data import fetch_test_data
 
-help_message = '''
+  help_message = '''
 
 This program is used to fetch xia2 example data from:
 
@@ -14,8 +15,8 @@ to xia2_regression - if the data are already there will not download again.
 
 '''
 
-if (len(sys.argv) < 2):
-  fetch_test_data()
-else:
-  print "Downloading into directory %s" % sys.argv[1]
-  fetch_test_data(sys.argv[1])
+  if len(sys.argv) < 2:
+    fetch_test_data()
+  else:
+    print "Downloading into directory %s" % sys.argv[1]
+    fetch_test_data(sys.argv[1])
